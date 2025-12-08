eae7e9a0f74010e2486371e5d076ee9ffd9e75b7
João Victor Gomes de Souza <gomesjoao2003@gmail.com>
2025-12-08 16:30:14 -0300

    Implementação do tratamento de Exceção nas classes

--- src/main/java/com/joao/myapi/services/UsuarioServices.java
+++ src/main/java/com/joao/myapi/services/UsuarioServices.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.joao.myapi.domain.Usuario;
 import com.joao.myapi.repositories.UsuarioRepository;
+import com.joao.myapi.services.exception.ObjectNotFoundException;
 
 @Service
 // informa para o spring que é a clçasse de serviços
@@ -17,7 +18,7 @@ public class UsuarioServices {
 	
 	public Usuario findById(Integer id) {
 		Optional<Usuario> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", tipo: " + Usuario.class.getName()));
 	}
 	
 }
